c78fb7f4dc641d7ab80f0a87182d55eb6da08f8a
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-04-14 10:02:31 -0500

    Introduction scene

--- Really/Program.cs
+++ Really/Program.cs
@@ -6,7 +6,23 @@ namespace Really
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("You are dreaming.\nYou see yourself as a child");
+            Console.WriteLine("You are alone in a sandbox, digging a hole with your hands");
+            Console.WriteLine("You watch in amazement as the edges of the hole begin to crumble inwards");
+            Console.WriteLine("Do you allow curiosity to get the best of you? ");
+            Console.ReadLine();
+            string response = Console.ReadLine();
+
+            if(response == "yes")
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Suddenly, the sand beneath you gives way . . . f");
+            }
+            else
+            {
+
+            }
         }
     }
 }
